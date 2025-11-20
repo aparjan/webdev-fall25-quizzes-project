ceb58f559413631432117f821f6ec295913093e9
aparjan <raji.aparnaa@gmail.com>
2025-11-20 01:28:19 -0500

    changed account details

--- app/Kambaz/Account/reducer.ts
+++ app/Kambaz/Account/reducer.ts
@@ -3,20 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const accountSlice = createSlice({
   name: "account",
   initialState: {
-    currentUser: {
-      _id: "234",
-      username: "dark_knight",
-      password: "wayne123",
-      firstName: "Bruce",
-      lastName: "Wayne",
-      email: "bruce@wayne.com",
-      dob: "1972-02-19",
-      role: "STUDENT",
-      loginId: "001234562S",
-      section: "S101",
-      lastActivity: "2020-11-02",
-      totalActivity: "15:32:43"
-    }
+    currentUser: null
   },
   reducers: {
     setCurrentUser: (state, action) => {
